1cc34d26f0ac9ddc0d5c797fc0acb069a311067f
DKAKI SALAHDDINE <136818132+SALAHDDINEDKAKI@users.noreply.github.com>
2025-07-13 14:51:38 +0200

    Delete .history/assets directory

--- .history/assets/js/script_20250712103622.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('./.text').forEach(el => {
-      el.addEventListener('copy', e => {
-        e.preventDefault();
-        alert('Copying is not allowed for this text.');
-      });
-    });
-  });
-  
--- .history/assets/js/script_20250712103630.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.contact-section .group .text').forEach(el => {
-      el.addEventListener('copy', e => {
-        e.preventDefault();
-        alert('Copying is not allowed for this text.');
-      });
-    });
-  });
-  
